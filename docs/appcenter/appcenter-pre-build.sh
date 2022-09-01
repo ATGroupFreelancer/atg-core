c5d78848a451d938439a1f4052506fedc8735de7
tuannguyen <npp.nguyentuan@gmail.com>
2022-09-01 22:35:40 +0700

    update docs

--- docs/appcenter/appcenter-pre-build.sh
+++ docs/appcenter/appcenter-pre-build.sh
@@ -19,17 +19,17 @@ GOOGLE_SERVICE_FILE_DEV=$APPCENTER_SOURCE_DIRECTORY/android/app/src/dev/google-s
 GOOGLE_SERVICE_FILE_STAG=$APPCENTER_SOURCE_DIRECTORY/android/app/src/stag/google-services.json
 GOOGLE_SERVICE_FILE=$APPCENTER_SOURCE_DIRECTORY/android/app/google-services.json
 
-IOS_GOOGLE_SERVICE_FILE=$APPCENTER_SOURCE_DIRECTORY/ios/GroveOne/GoogleService-Info.plist
-IOS_GOOGLE_SERVICE_FILE_DEV=$APPCENTER_SOURCE_DIRECTORY/ios/GroveOne/GoogleServices/Qa/GoogleService-Info.plist
-IOS_GOOGLE_SERVICE_FILE_STAG=$APPCENTER_SOURCE_DIRECTORY/ios/GroveOne/GoogleServices/Staging/GoogleService-Info.plist
+IOS_GOOGLE_SERVICE_FILE=$APPCENTER_SOURCE_DIRECTORY/ios/<Project Name>/GoogleService-Info.plist
+IOS_GOOGLE_SERVICE_FILE_DEV=$APPCENTER_SOURCE_DIRECTORY/ios/<Project Name>/GoogleServices/Qa/GoogleService-Info.plist
+IOS_GOOGLE_SERVICE_FILE_STAG=$APPCENTER_SOURCE_DIRECTORY/ios/<Project Name>/GoogleServices/Staging/GoogleService-Info.plist
 
-IOS_INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/ios/GroveOne/Info.plist
+IOS_INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/ios/<Project Name>/Info.plist
 
 cat ./src/configuration/env-configs/prod.ts
 
 if [ "$APPCENTER_BRANCH" == "develop" ];
 then
-    sed 's/host\: .*/host\: `http:\/\/[SUB_DOMAIN].qa.grovehr.com\/api`,/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
+    sed 's/host\: .*/host\: `http:\/\/[SUB_DOMAIN].qa.<Backend Domain>.com\/api`,/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
     sed 's/buildEnv\: .*/buildEnv\: "dev",/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
     sed 's/mixpanel_token\: .*/mixpanel_token\: "16cfc8a69b159ae9a7e073104a1546ec",/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
     sed -n 'p' $GOOGLE_SERVICE_FILE_QA > $GOOGLE_SERVICE_FILE
@@ -41,45 +41,8 @@ then
     #IOS
     if [ -e "$IOS_INFO_PLIST_FILE" ]
     then
-        plutil -replace CFBundleDisplayName -string "GroveQA" $IOS_INFO_PLIST_FILE
-        plutil -replace CFBundleIdentifier -string "com.grovedev" $IOS_INFO_PLIST_FILE
-        echo 'Info plist content:'
-        cat $IOS_INFO_PLIST_FILE
-    fi
-fi
-
-if [ "$APPCENTER_BRANCH" == "pre-production" ];
-then
-    sed 's/host\: .*/host\: `http:\/\/[SUB_DOMAIN].stag.grovehr.com\/api`,/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
-    sed 's/buildEnv\: .*/buildEnv\: "stagging",/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
-    sed 's/mixpanel_token\: .*/mixpanel_token\: "16cfc8a69b159ae9a7e073104a1546ec",/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
-    sed -n 'p' $GOOGLE_SERVICE_FILE_STAG > $GOOGLE_SERVICE_FILE
-    echo "Updating Android Google Services to $GOOGLE_SERVICE_FILE in environments.ts for Stag"
-     sed -n 'p' $IOS_GOOGLE_SERVICE_FILE_STAG > $IOS_GOOGLE_SERVICE_FILE
-    echo "Updating Ios Google Services to $GOOGLE_SERVICE_FILE in environments.ts for Stag"
-    #IOS
-    if [ -e "$IOS_INFO_PLIST_FILE" ]
-    then
-        plutil -replace CFBundleDisplayName -string "GroveStag" $IOS_INFO_PLIST_FILE
-        plutil -replace CFBundleIdentifier -string "com.grovestag" $IOS_INFO_PLIST_FILE
-        echo 'Info plist content:'
-        cat $IOS_INFO_PLIST_FILE
-    fi
-fi
-
-if [ "$APPCENTER_BRANCH" == "GO-472" ];
-then
-    sed 's/host\: .*/host\: `http:\/\/[SUB_DOMAIN].qa.grovehr.com\/api`,/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
-    sed 's/buildEnv\: .*/buildEnv\: "dev",/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
-    sed 's/mixpanel_token\: .*/mixpanel_token\: "16cfc8a69b159ae9a7e073104a1546ec",/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
-    sed -n 'p' $GOOGLE_SERVICE_FILE_QA > $GOOGLE_SERVICE_FILE
-    echo "Updating Android Google Services to $GOOGLE_SERVICE_FILE in environments.ts for QA"
-
-    #IOS
-    if [ -e "$IOS_INFO_PLIST_FILE" ]
-    then
-        plutil -replace CFBundleDisplayName -string "Grove Dev" $IOS_INFO_PLIST_FILE
-        plutil -replace CFBundleIdentifier -string "com.grovedev" $IOS_INFO_PLIST_FILE
+        plutil -replace CFBundleDisplayName -string "<App Name With Branch Dev>" $IOS_INFO_PLIST_FILE
+        plutil -replace CFBundleIdentifier -string "<Bundle Id With Branch Dev>" $IOS_INFO_PLIST_FILE
         echo 'Info plist content:'
         cat $IOS_INFO_PLIST_FILE
     fi
@@ -87,7 +50,7 @@ fi
 
 if [ "$APPCENTER_BRANCH" == "production" ];
 then
-    sed 's/host\: .*/host\: `https:\/\/[SUB_DOMAIN].grovehr.com\/api`,/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
+    sed 's/host\: .*/host\: `https:\/\/[SUB_DOMAIN].<Backend Domain>.com\/api`,/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
     sed 's/buildEnv\: .*/buildEnv\: "prod",/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
     sed 's/mixpanel_token\: .*/mixpanel_token\: "7e3ee6a6a6cb0da0a3d8c7893b04d18d",/g' ./src/configuration/env-configs/prod.ts > ./tmp.ts && mv ./tmp.ts ./src/configuration/env-configs/prod.ts
 fi
